2faab29a1125bb40ad1ef0764a64fbbfee211fd7
waldivamweb21 <waldivamprogramadorweb21@gmail.com>
2025-10-25 18:02:43 -0300

    base criadas

---
+++ src/routes/index.ts
@@ -0,0 +1,22 @@
+import { Router } from "express";
+import * as PageController from '../controllers/pageController';
+import * as searchController from '../controllers/searchController';
+
+const router = Router();
+
+/*
+router.get('/', (req, res)=>{
+    res.send('home');
+});
+*/
+
+router.get('/', PageController.home);
+router.get('/dogs', PageController.dogs);
+router.get('/cats', PageController.cats);
+router.get('/fishes', PageController.fishes);
+
+router.get('/search', searchController.search);
+
+export default router;
+
+
